79d51892228efb835676b49e0b3c293b512acab7
Carlos <carlos.mastrangelo@gmail.com>
2024-01-12 23:52:28 -0800

    feat: implemented get_latest_prices

--- soozpy/queries.py
+++ soozpy/queries.py
@@ -1,10 +1,35 @@
-from soozpy.db_access import query_historical_prices
+from soozpy.db_access import query_historical_prices, query_latest_prices
 import pandas as pd
 
 
 def get_historical_prices(from_date, to_date, symbols=None):
     # Call the function from db_access.py to get the query results
     results = query_historical_prices(from_date, to_date, symbols)
+
+    # Convert the results to a DataFrame
+    if results:
+        df = pd.DataFrame([{
+            'symbol': r.symbol,
+            'datetime': r.datetime,
+            'price': r.price,
+            'volume': r.volume_24h
+        } for r in results])
+    else:
+        df = pd.DataFrame()
+
+    return df
+
+
+def get_latest_prices(symbols=None):
+    """
+    Retrieve the latest price data for given symbols or all symbols if none are specified,
+    and return the data as a pandas DataFrame.
+
+    :param symbols: Optional list of symbols to query for. If None, queries for all symbols.
+    :return: pandas DataFrame containing the latest price data.
+    """
+    # Call the function to get the latest prices
+    results = query_latest_prices(symbols)
 
     # Convert the results to a DataFrame
     if results:
